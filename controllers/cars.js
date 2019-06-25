453ea26e9ba19ffa6c47139accacfcbfff53e2bf
RomanBogatikov <ivaroman2000@gmail.com>
2019-06-25 18:36:14 -0400

    added images of cars to display

--- controllers/cars.js
+++ controllers/cars.js
@@ -1,8 +1,17 @@
-const express = require('espress');
+const express = require('express');
 const cars = express.Router();
 const Car = require('../models/Car');
 const mongoose = require('mongoose');
 
-
+cars.get('/', (req, res) => {
+// get all the cars from the database and send to the Client
+  Car.find({}, (error, allCars) => {
+    if (error) {
+      res.status(500).send('internal error, cannot fetch cars from the database')
+    } else {
+      res.status(200).send(allCars);
+    }
+  })
+})
 
 module.exports = cars;
